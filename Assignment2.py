cf6f418b9e8f48f09c0dfd07cd8d64d9f4e1b340
Matthias Claeys <matthias.claeys2002@gmail.com>
2024-06-03 11:59:36 +0200

    added functions for Assignment2.py

--- Assignment2.py
+++ Assignment2.py
@@ -21,13 +21,21 @@ rcut_21 = 14
 ncycles21 = 500000
 npart = 362
 
-delta_arr = np.linspace(0.005, 1, 25)
+# question21(T_21, side_length_21, rcut_21, ncycles21)
+
+# question 2.2
+T_22 = 400
+side_length_22 = 75
+rcut_22 = 30
+# question22(T_22, side_length_22, rcut_22)
+
 
 def create_file_names(folder: str, delta: float, acceptance_ratio: float):
     rounded = f"{delta:.7f}"  # Round to 7 decimal places
     without_leading_zero = rounded[2:]  # Remove the leading "0."
     formatted_name = folder + without_leading_zero.ljust(7, '0') + ".csv"  # Pad with zeros if necessary and add ".csv"
     return formatted_name
+
 
 def question21(T, side_length, rcut, ncycles):
     delta_arr = np.linspace(0.005, 1, 25)
@@ -52,33 +60,30 @@ def question21(T, side_length, rcut, ncycles):
                     csv_writer.writerow([delta, E_ave[j], P_ave[j], acceptance_ratio])
         end = time.time()
 
-        print(f"{i}: Delta= {round(delta, 5)}, acceptance ratio = {round(acceptance_ratio, 5)}, duration = {round(end - start, 5)} seconds \n")
+        print(
+            f"{i}: Delta= {round(delta, 5)}, acceptance ratio = {round(acceptance_ratio, 5)}, duration = {round(end - start, 5)} seconds \n")
     print("excersice 2.1 done")
-
-
-def get_num_molecules(density: float, side_length: float, molar_mass: float) -> float:
-    return math.ceil((density * side_length**3 / molar_mass) * const.N_A)
 
 
 def question22(T, side_length, rcut):
     print("exercise 2.2: liquid system")
-    delta_arr = np.linspace(0.005, 1, 25)
+    delta_arr = np.linspace(0.005, side_length / 2, 25)
     density = 9.68
-    num_molecules = get_num_molecules(density, side_length, 16.04)
+    num_molecules = get_num_molecules(density, side_length, 16.04 / 1000)
+    print(f"num_molecules: {num_molecules}")
 
     directory = 'Ass22Outputs/' + str(datetime.now().strftime("%Y-%m-%d_%H-%M-%S")) + '/'
     os.mkdir(directory)
 
     for (i, delta) in enumerate(delta_arr):
-        print(i, delta)
+
         start = time.time()
         mc_21 = Simulation.MonteCarloSimulation(temp=T, side_length=side_length, rcut=rcut,
                                                 eta_kb=eta_kb, sigma=sigma, delta=delta, npart=num_molecules)
 
-
         E_ave, P_ave, acceptance_ratio = mc_21.run()
 
-        filename = create_file_names(directory, delta)
+        filename = create_file_names(directory, delta, acceptance_ratio)
         fields = ['Delta', 'Energy', 'Pressure', 'Acceptance Ratio']
         with open(filename, mode='w', newline='') as csfile:
             csv_writer = csv.writer(csfile, delimiter=',', quotechar='"')
@@ -93,6 +98,39 @@ def question22(T, side_length, rcut):
     print("excersice 2.2 done")
 
 
+# question22(400, 75, 30)
+
+
+# question 2.3
+def question23(T, side_length, rcut):
+    print("exercise 2.3: solid system")
+    delta_arr = np.linspace(0.005, 1, 25)
+
+    directory = 'Ass23Outputs/' + str(datetime.now().strftime("%Y-%m-%d_%H-%M-%S")) + '/'
+    os.mkdir(directory)
+    for (i, delta) in enumerate(delta_arr):
+        print(i, delta)
+        start = time.time()
+        mc_23 = Simulation.MonteCarloSimulation(temp=T, side_length=side_length, rcut=rcut,
+                                                eta_kb=eta_kb, sigma=sigma, ncycle=ncycles, delta=delta)
+        E_ave, P_ave, acceptance_ratio = mc_21.run(start_conf=False)
+
+        filename = create_file_names(directory, delta)
+        fields = ['Delta', 'Energy', 'Pressure', 'Acceptance Ratio']
+        with open(filename, mode='w', newline='') as csfile:
+            csv_writer = csv.writer(csfile, delimiter=',', quotechar='"')
+            csv_writer.writerow(fields)
+            for j in range(ncycles21):
+                if E_ave[j] != 0 and P_ave[j] != 0:
+                    csv_writer.writerow([delta, E_ave[j], P_ave[j], acceptance_ratio])
+        end = time.time()
+
+        print(
+            f"{i}: Delta= {round(delta, 5)}, acceptance ratio = {round(acceptance_ratio, 5)}, duration = {round(end - start, 5)} seconds \n")
+    print("excersice 2.3 done")
+
+# question23(T_21, side_length_21, rcut_21)
+
 def get_acceptance_ratio(directory: str):
     acceptance_ratios = []
     deltas = []
@@ -104,5 +142,23 @@ def get_acceptance_ratio(directory: str):
             deltas.append(df['Delta'][0])
     return np.array(acceptance_ratios), np.array(deltas)
 
+# plot acceptance ratio vs delta
+def plot_acceptance_ratio_vs_delta(directory: str, savefig: bool = False) -> None:
+    fig = plt.figure(figsize=(10, 5))
+    ax = fig.add_subplot(111)
+    acceptance_ratio, delta = get_acceptance_ratio(directory)
+    ax.scatter(delta, acceptance_ratio)
 
+    for (i, delta) in enumerate(delta):
+        ax.annotate(f"{delta:.3f}", (delta, acceptance_ratio[i]))
+    ax.set_xlabel('Delta')
+    ax.set_ylabel('Acceptance Ratio')
+    ax.set_title('Acceptance Ratio vs Delta')
+    if savefig:
+        plt.savefig( str(datetime.now().strftime("%Y-%m-%d_%H-%M-%S")) + 'AcceptanceRatioVsDelta.png')
+    plt.grid()
+    plt.show()
 
+# plot_acceptance_ratio_vs_delta('Ass21Outputs/2024-05-31_16-51-56/')
+def get_num_molecules(density: float, side_length: float, molar_mass: float) -> float:
+    return math.ceil((density * (side_length * 10 ** -10) ** 3 / molar_mass) * const.N_A)
